69b0dd213eead9dad3f5721aa26678d605541de7
amayaoriana-droid <amayaorianamzapata@gmail.com>
2025-10-13 10:20:58 -0300

    Proyecto vacio
    nuevo cambio

---
+++ src/main/java/oriseba/Lunacafe/LunacafeApplication.java
@@ -0,0 +1,13 @@
+package oriseba.Lunacafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunacafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunacafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oriseba/Lunacafe/LunacafeApplicationTests.java
@@ -0,0 +1,13 @@
+package oriseba.Lunacafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunacafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
