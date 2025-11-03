1e082246fc711c3bce1fc91815c9c5f654e289dc
amayaoriana-droid <98384850+Venom9879@users.noreply.github.com>
2025-11-03 11:09:47 -0300

    Borrar

--- src/main/java/oriseba/Lunacafe/Repositorio/PagoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package oriseba.Lunacafe.Repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import oriseba.Lunacafe.entity.Pago;
-
-public interface PagoRepositorio extends JpaRepository <Pago, Long>{
-
-}
