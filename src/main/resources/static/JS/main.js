0d2a128b1481145f12c2798fc23f119c8256d0f5
amayaoriana-droid <230906871+amayaoriana-droid@users.noreply.github.com>
2025-11-10 10:34:24 -0300

    Borrar errores
    Subi html, css y js sin querer

--- src/main/resources/static/JS/main.js
+++
@@ -1,8 +0,0 @@
-// Luna Café - funciones simples
-console.log("🌙 Luna Café iniciado correctamente");
-
-// Podés agregar interacción simple:
-document.addEventListener("submit", e => {
-  e.preventDefault();
-  alert("¡Gracias por tu mensaje! Te responderemos pronto ☕");
-});
